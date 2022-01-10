6724bd3c5e6a5af9fc231f9723d476c097a53204
kitanoshohei <kitano.1105.shohei2598@gmail.com>
2022-01-10 21:41:24 +0900

    2章完了、3章初め

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,17 +2,48 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h1>{{ inputText }}</h1>
+    <p v-bind:style="style">v-bind テスト</p>
+    <p v-bind:class="{testRed:isRed, testBlue:isBlue}">v-bind class テスト</p>
+    <p v-bind:class="classes">classes テスト</p>
+    <p v-if="isIf">v-if test</p>
+    <p v-else>v-else test</p>
+    <ul>
+      <li v-for="item in items" :key="item.id">{{item.name}}</li>
+    </ul>
+    <Test msg="props test"/>
+    <!-- propsで値を渡したいときには":"をつけるかv-bind:をつける-->
+    <Test v-for="item in items" :key="item.id" :msg="item.name"/>
     <input type="text" @input="doInput">
-    <button @click="doAction()">change</button>
+    <button @click="doAction">onclick change</button>
+    <button @click="doChangeStyle">change style</button>
+    <button @click="doChangeStyleClass">change Class</button>
+    <button @click="doChangeClasses">change Classes</button>
+    <button @click="doChangeIf">change v-if</button>
   </div>
 </template>
 
 <script>
+import Test from "./Test"
 export default {
   name: 'HelloWorld',
+  components: {
+    Test,
+  },
   data: () => ({
     msg: 'kitanoshohei',
     inputText: 'null',
+    style:'color:black',
+    isRed:true,
+    isBlue:false,
+    classes:{
+      testRed:true,
+      testBlue:false,
+    },
+    isIf:true,
+    items:[
+      {id:1, name:'orange'},
+      {id:2, name:'apple'},
+    ]
   }),
   methods: {
     doAction () {
@@ -20,6 +51,20 @@ export default {
     },
     doInput (event) {
       this.inputText = event.target.value;
+    },
+    doChangeStyle () {
+      this.style = 'color:red'
+    },
+    doChangeStyleClass () {
+      this.isRed = false;
+      this.isBlue = true;
+    },
+    doChangeClasses () {
+      this.classes.testRed = !this.classes.testRed;
+      this.classes.testBlue = !this.classes.testBlue;
+    },
+    doChangeIf () {
+      this.isIf = !this.isIf;
     }
   }
 }
@@ -27,4 +72,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .testRed {
+    color:red;
+  }
+  .testBlue {
+    color:blue;
+  }
 </style>
---
+++ src/components/Test.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props:['msg'],
+  data: () => ({
+
+  }),
+  methods: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .testRed {
+    color:red;
+  }
+  .testBlue {
+    color:blue;
+  }
+</style>
